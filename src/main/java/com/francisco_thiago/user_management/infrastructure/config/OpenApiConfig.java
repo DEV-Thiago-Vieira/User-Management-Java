6ffb38f05638394ceda56a48b78cd1b2fe9af7f9
Thiago Vieira <pessoalvieira779@gmail.com>
2026-05-15 14:27:40 -0300

    feat: basic documentation configuration class

---
+++ src/main/java/com/francisco_thiago/user_management/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,54 @@
+package com.francisco_thiago.user_management.infrastructure.config;
+
+//import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+//import io.swagger.v3.oas.models.security.SecurityRequirement;
+//import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        Server local = new Server();
+        local.setUrl("http://localhost:8081");
+        local.setDescription("Local environment");
+
+//        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Restaurant Management API")
+                        .version("1.0")
+                        .description("API documentation for restaurant management")
+                        .contact(new Contact()
+                                .name("Thiago Vieira")
+                                .email("vieirathiago779@gmail.com")
+                                .url("https://thiago-vieira.com"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://github.com/DEV-Thiago-Vieira/User-Management-Java"))
+                )
+                .servers(List.of(local));
+//              AUTHORIZATION SETTINGS
+//                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+//                .components(new Components()
+//                        .addSecuritySchemes(securitySchemeName,
+//                                new SecurityScheme()
+//                                        .name(securitySchemeName)
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")
+//                        )
+//                );
+
+    }
+}
