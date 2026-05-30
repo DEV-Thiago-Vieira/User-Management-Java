44397ce096b003c47a3cd6ec8d4d66a3f57e423f
Thiago Vieira <pessoalvieira779@gmail.com>
2026-05-30 10:08:16 -0300

    build: security and jwt added

--- src/main/java/com/francisco_thiago/user_management/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/francisco_thiago/user_management/infrastructure/config/OpenApiConfig.java
@@ -1,12 +1,12 @@
 package com.francisco_thiago.user_management.infrastructure.config;
 
-//import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-//import io.swagger.v3.oas.models.security.SecurityRequirement;
-//import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +22,7 @@ public class OpenApiConfig {
         local.setUrl("http://localhost:8081");
         local.setDescription("Local environment");
 
-//        final String securitySchemeName = "bearerAuth";
+        final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
                 .info(new Info()
@@ -37,18 +37,16 @@ public class OpenApiConfig {
                                 .name("MIT License")
                                 .url("https://github.com/DEV-Thiago-Vieira/User-Management-Java"))
                 )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
                 .servers(List.of(local));
-//              AUTHORIZATION SETTINGS
-//                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-//                .components(new Components()
-//                        .addSecuritySchemes(securitySchemeName,
-//                                new SecurityScheme()
-//                                        .name(securitySchemeName)
-//                                        .type(SecurityScheme.Type.HTTP)
-//                                        .scheme("bearer")
-//                                        .bearerFormat("JWT")
-//                        )
-//                );
-
     }
 }
